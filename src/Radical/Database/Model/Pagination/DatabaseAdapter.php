fa82ce293ad288c7b6e0c63846af2b1ce42cad84
splitice <mat999@gmail.com>
2014-11-25 19:45:53 +1100

    factored out generic pagination

--- src/Radical/Database/Model/Pagination/DatabaseAdapter.php
+++ src/Radical/Database/Model/Pagination/DatabaseAdapter.php
@@ -9,6 +9,30 @@
 namespace Radical\Database\Model\Pagination;
 
 
-class DatabaseAdapter {
+use Radical\Database\Model\Table\TableSet;
+use Radical\Web\Pagination\IPaginationSource;
+use Radical\Database\SQL;
 
+class DatabaseAdapter implements IPaginationSource {
+    private $source;
+    private $sql;
+
+    function __construct(TableSet $set){
+        $this->source = $set;
+        $this->sql = new SQL\SelectStatement(null,null);
+    }
+    function get_where($field, $value){
+        $sql = clone $this->sql;
+        $sql->where_and(array($field,'LIKE',$value.'%'));
+        return $this->source->Filter($sql);
+    }
+    function get_limit($start, $count){
+        $sql = clone $this->sql;
+        $sql->limit($start, $count);
+
+        return $this->source->Filter($sql);
+    }
+    function getCount(){
+        return $this->source->getCount();
+    }
 } 
--- src/Radical/Database/Model/Pagination/IDatabasePaginator.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Radical\Database\Model\Pagination;
-
-use Radical\Pagination\IPaginator;
-use Radical\Pagination\Template\IPaginationTemplate;
-
-interface IDatabasePaginator extends \IteratorAggregate {
-	function outputLinks(IPaginator $paginator,IPaginationTemplate $template);
-}
